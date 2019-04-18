e897375bd7b6c617b6bf0628a56b0f37555f089a
Claire <claire.capelo@hotmail.com>
2019-04-18 17:09:05 +0200

    cont

--- Project1/ConvNet1.py
+++ Project1/ConvNet1.py
@@ -1,10 +1,25 @@
 from torch import nn
 from torch.nn import functional as F
 
-class ConvNet1(nn.Module):
+class ConvNet1_1C(nn.Module):
     def __init__(self, nb_classes, nb_hidden=200):
-        super(ConvNet1, self).__init__()
+        super(ConvNet1_1C, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.fc1 = nn.Linear(256, nb_hidden)
+        self.fc2 = nn.Linear(nb_hidden, nb_classes)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=2, stride=2))
+        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2, stride=2))
+        x = F.relu(self.fc1(x.view(-1, 256)))
+        x = self.fc2(x)
+        return x
+    
+class ConvNet1_2C(nn.Module):
+    def __init__(self, nb_classes, nb_hidden=200):
+        super(ConvNet1_2C, self).__init__()
+        self.conv1 = nn.Conv2d(2, 32, kernel_size=3)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
         self.fc1 = nn.Linear(256, nb_hidden)
         self.fc2 = nn.Linear(nb_hidden, nb_classes)
